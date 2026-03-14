bc20aefee6190bef84c2bf711255b00b40b86197
Ad Soyad <mail@ornek.com>
2026-03-14 14:50:52 +0300

    MANUEL YEDEK: 14031450

--- m_y_bend.bat
+++ m_y_bend.bat
@@ -2,18 +2,17 @@
 set /p mesaj="Yedekleme notunuzu girin: "
 echo [%date% %time%] Bekent yedekleme baslatiliyor...
 
-:: 1. SQL Yedek Klasör Kontrolü
+:: 1. SQL Yedekleme Bölümü
 if not exist "..\bekent-sql-yedekler" mkdir "..\bekent-sql-yedekler"
 set PGPASSWORD=123456
 
-:: EN ÖNEMLİ YER: Tırnak işaretlerini ekledim ki boşluklardan dolayı "yolu bulamıyor" demesin.
-set "PG_DUMP_EXE=C:\Program Files\PostgreSQL\16\bin\pg_dump.exe"
-set "OUTPUT_FILE=..\bekent-sql-yedekler\db_yedek_guncel.sql"
+echo SQL yedekleniyor... lütfen bekleyin...
 
-echo SQL yedekleniyor...
-"%PG_DUMP_EXE%" -U postgres -d teknik_servis_db > "%OUTPUT_FILE%"
+:: Yolu komple tırnak içine alıp komutu direkt çağırıyoruz
+set "PG_DUMP_PATH=C:\Program Files\PostgreSQL\16\bin\pg_dump.exe"
+"%PG_DUMP_PATH%" -U postgres -d teknik_servis_db -f "..\bekent-sql-yedekler\db_yedek_guncel.sql"
 
-:: 2. GitHub Yedek
+:: 2. GitHub Yedekleme Bölümü
 echo GitHub'a gonderiliyor...
 git add .
 git commit -m "MANUEL YEDEK: %mesaj%"
@@ -21,6 +20,6 @@ git push origin main
 
 echo.
 echo ===========================================
-echo   ISLEM TAMAM! SQL VE KODLAR GUVENDE.
+echo   MUDURUM ISLEM TAMAM! SARI YAZI SUSTU.
 echo ===========================================
 pause
