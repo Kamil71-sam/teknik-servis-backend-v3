f1ffbe6c73f9e44c78492d0ff330bd5e337a828a
Ad Soyad <mail@ornek.com>
2026-03-14 15:13:54 +0300

    MANUEL YEDEK: 14031513

--- m_y_bend.bat
+++ m_y_bend.bat
@@ -2,22 +2,16 @@
 set /p mesaj="Yedekleme notunuzu girin: "
 echo [%date% %time%] Bekent yedekleme baslatiliyor...
 
-:: 1. SQL Yedek Bölümü
+:: 1. SQL Yedekleme Bölümü
 if not exist "..\bekent-sql-yedekler" mkdir "..\bekent-sql-yedekler"
 set PGPASSWORD=123456
 
 echo SQL yedekleniyor... lutfen bekleyin...
 
-:: Önce PostgreSQL klasörüne giriyoruz (En garanti yol budur müdürüm)
-cd /d "C:\Program Files\PostgreSQL\16\bin"
+:: Yolu senin bilgisayardaki gercek yerine (C:\pgdata) gore ayarladim
+"C:\pgdata\bin\pg_dump.exe" -U postgres -d teknik_servis_db -f "..\bekent-sql-yedekler\db_yedek_guncel.sql"
 
-:: Şimdi komutu "evindeymiş gibi" çalıştırıyoruz
-pg_dump.exe -U postgres -d teknik_servis_db -f "%~dp0..\bekent-sql-yedekler\db_yedek_guncel.sql"
-
-:: Tekrar dükkan klasörüne geri dönüyoruz
-cd /d "%~dp0"
-
-:: 2. GitHub Bölümü
+:: 2. GitHub Yedekleme Bölümü
 echo GitHub'a gonderiliyor...
 git add .
 git commit -m "MANUEL YEDEK: %mesaj%"
@@ -25,6 +19,6 @@ git push origin main
 
 echo.
 echo ===========================================
-echo   MUDURUM BU SEFER CORTLADI! SİSTEM TAMAM.
+echo   OPERASYON TAMAM! ARTIK GUVENDESIN MUDURUM.
 echo ===========================================
 pause
